e8b385ee66ef4137ec7b580a78263ee2746ac0fb
Amey Kusurkar <amey1000@gmail.com>
2022-03-31 22:08:18 +0100

    Pass 2nd test for builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,16 @@
 use proc_macro::TokenStream;
-// use quote::quote;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    let expanded = quote! {
+        impl Command {
+            pub fn builder() {}
+        }
+    };
+
+    TokenStream::from(expanded)
 }
