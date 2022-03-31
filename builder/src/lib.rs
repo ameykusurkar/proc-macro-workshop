29177d4ddc78ec11b587c0712b87a121a6f362be
Amey Kusurkar <amey1000@gmail.com>
2022-03-31 22:30:32 +0100

    Pass 3nd test for builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -25,6 +25,24 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 }
             }
         }
+        impl #b_ident {
+            fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+        }
     };
 
     TokenStream::from(expanded)
