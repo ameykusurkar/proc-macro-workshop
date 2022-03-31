d074b3f7db7769bf34e2187e6b209ec415e9a1de
Amey Kusurkar <amey1000@gmail.com>
2022-03-31 22:32:25 +0100

    Simplify

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -6,7 +6,7 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let ident = &ast.ident;
-    let b_ident = syn::Ident::new(&format!("{}Builder", ident.to_string()), ident.span());
+    let b_ident = syn::Ident::new(&format!("{}Builder", ident), ident.span());
 
     let expanded = quote! {
         pub struct #b_ident {
